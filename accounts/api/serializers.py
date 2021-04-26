6b42e630aca8003e128a42b3a34f559ef0346d59
Alfred L <alliu930410@gmail.com>
2021-04-26 09:30:16 -0400

    Login, Login Status, Logout, Signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,58 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework import exceptions
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                "username": "User does not exist."
+            })
+        return data
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+    # TODO: add display name to the User model
+    # displayname = serializers.CharField(max_length=20, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        password = validated_data['password'].lower()
+        email = validated_data['email'].lower()
+        # displayname = validated_data['username']
+
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            email=email,
+            # displayname=displayname
+        )
+        return user
